e9cf0a95bda7a0637706ddf96c02fdd4dbe51621
WenheLi <[wl1508@nyu.edu]>
2018-02-07 12:31:53 +0800

    restructure the json file && add forget pwd

--- auth/auth.py
+++ auth/auth.py
@@ -11,7 +11,6 @@ from utli import send_mail
 def login():
     if request.method == 'POST':
         data = request.get_json(force=True)
-        print(data)
 
         user = Users.query.filter_by(email=data['email']).first()
 
@@ -45,7 +44,7 @@ def signup():
         data = request.get_json(force=True)
 
         q = Users.query.filter_by(email=data['email']).first()
-        if q is not None:
+        if q is None:
             user = Users(email=data['email'], pwd=data['pwd'], gender=data['gender'])
             db.session.add(user)
             db.session.commit()
@@ -53,14 +52,45 @@ def signup():
             rec = [data['email']]
             content = 'steins.xin:8001/auth/verify/' + data['email']
             send_mail(subject=subject, recv=rec, content=content)
-            return jsonify(dict(status=0))
-        else:
             return jsonify(dict(status=1))
+        else:
+            return jsonify(dict(status=0))
+
+
+@auth.route('/forgetpwd', methods=['POST'])
+def forget_pwd():
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+
+        q = Users.query.filter_by(email=data['email']).first()
+        if q is not None:
+            subject = 'Change your password'
+            rec = [data['email']]
+            content = 'steins.xin:8001/auth/forget/' + data['email']
+            send_mail(subject=subject, recv=rec, content=content)
+            return jsonify(dict(status=1))
+        else:
+            return jsonify(dict(status=0))
 
 
 @auth.route('/verify/<username>', methods=['GET'])
 def check(username):
-    print(username)
+    user = Users.query.filter_by(email=username).first()
+    if user is not None:
+        if not user.is_valid:
+            user.is_valid = True
+            db.session.add(user)
+            db.session.commit()
+            return 'cong! U have activate this email'
+        else:
+            return 'you have done this already'
+    else:
+        return 'please check the link if it is right'
+
+
+# TODO: Website!
+@auth.route('/forget/<username>', methods=['GET'])
+def get_pwd(username):
     user = Users.query.filter_by(email=username).first()
     if user is not None:
         if not user.is_valid:
