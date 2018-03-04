ba835c3dc7c9a227ee1e99b3c8b528a5e9a62530
WenheLi <[wl1508@nyu.edu]>
2018-03-04 15:22:23 +0800

    hopefully work

--- personal/personal.py
+++ personal/personal.py
@@ -4,8 +4,8 @@ from models.Users import Users
 from personal import personal
 from flask_login import logout_user, login_required
 
-@login_required
 @personal.route('/fetchavatar/<username>', methods=['GET', 'POST'])
+@login_required
 def login():
     if request.method == 'POST':
         return 'hello'
